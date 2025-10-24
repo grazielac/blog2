ac81b172ac9c394eddbe038e79105b1dbcfafec0
grazielac <cpgraziela@gmail.com>
2025-10-24 18:54:43 +0100

    Create a BlogCard component, a reusable card

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -8,10 +8,13 @@ interface BlogCardProps {
     date: string;
 }
 
-function BlogCard({ title, img, descrciption, author, date }: BlogCardProps) {
+function BlogCard({ title, img, description, author, date }: BlogCardProps) {
   return (
-    <div className="bg-white p-">
-      
+    <div className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition duration-200 border border-gray-100">
+      <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+      <p className="text-gray-400 mb-3">{description}</p>
+      <p className="text-gray-400 mb-3">{author}</p>
+      <p className="text-sm text-gray-400">{date}</p>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Nav from "./components/nav";
 import Feed from "./components/Feed";
+import BlogCard from "./components/BlogCard";
 
 export default function Home() {
   return (
     <>
      <Nav />
      <Feed />
+     <BlogCard />
+
     </>
   );
 }
