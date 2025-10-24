fa449332e605914029e1915e71e0c06df5720ec5
grazielac <cpgraziela@gmail.com>
2025-10-24 22:29:34 +0100

    Add Personal page route

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Nav() {
   return (
     <div>
-      <div className="text-sm flex gap-4 justify-around border-t-gray-400 border-b-gray-400 border-t-1 border-b-1 pt-8 pb-7">
+      <div className="text-sm flex gap-4 justify-around border-t-gray-400 border-b-gray-400 border-t border-b pt-8 pb-7">
         <Link href="/">Home</Link>
         <span>/</span>
         <Link href="/">Personal</Link>
