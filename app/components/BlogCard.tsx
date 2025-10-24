4a0ec100a297dee7446a354c2ff7ad557b8d46c9
grazielac <cpgraziela@gmail.com>
2025-10-24 18:50:03 +0100

    Define an interface - user objects

---
+++ app/components/BlogCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface BlogCardProps {
+    title: string;
+    img: url;
+    description: string;
+    author: string;
+    date: string;
+}
+
+function BlogCard({ title, img, descrciption, author, date }: BlogCardProps) {
+  return (
+    <div className="bg-white p-">
+      
+    </div>
+  )
+}
+
+export default BlogCard
