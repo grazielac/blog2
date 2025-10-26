0912fadf8230268ad943c0e9f5b21a21253aece6
grazielac <cpgraziela@gmail.com>
2025-10-26 10:08:32 +0000

    Move Link only to Image and 'read me'

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -25,41 +25,51 @@ function BlogCard({
   bgColor,
 }: BlogCardProps) {
   return (
-    <Link href={`/blog/${slug}`}>
-      <div style={{ backgroundColor: bgColor }} className="flex flex-col h-full border bg-white p-5 hover:shadow-lg transition duration-200 border-gray-100">
-        <div className="flex flex-col items-center text-center flex-1">
-          {/* title */}
-          <h2 className={`${cormorant.className} pt-10 line-clamp-2 text-2xl text-center text-gray-700 font-semibold mb-2`}>
-            {title}
-          </h2>
+    <div
+      style={{ backgroundColor: bgColor }}
+      className="flex flex-col h-full border bg-white p-5 hover:shadow-lg transition duration-200 border-gray-100"
+    >
+      <div className="flex flex-col items-center text-center flex-1">
+        {/* title */}
+        <h2
+          className={`${cormorant.className} pt-10 line-clamp-2 text-2xl text-center text-gray-700 font-semibold mb-2`}
+        >
+          {title}
+        </h2>
 
-          {/* image */}
+        {/* image */}
+        <Link href={`/blog/${slug}`}>
           <div className="relative mb-4 w-full h-120 overflow-hidden p-10 pt-4">
-            <Image src={image} alt={title} className="object-cover w-full h-full rounded-lg" />
+            <Image
+              src={image}
+              alt={title}
+              className="object-cover w-full h-full rounded-lg"
+            />
           </div>
+        </Link>
 
-          {/* description */}
-          <p className="text-gray-600 text-sm mb-4 line-clamp-3 text-center">{description}</p>
-          <p className="text-gray-500 text-sm line-clamp-4 text-center">
-             {date}
-          </p>
+        {/* description */}
+        <p className="text-gray-600 text-sm max-w-sm mb-4 line-clamp-3 text-center">
+          {description}
+        </p>
+        <p className="text-gray-500 text-sm line-clamp-4 text-center">{date}</p>
 
-          <div className="pt-10 flex flex-wrap items-center gap-12 justify-around">
-            {tags &&
-              tags.map((tag, index) => (
-                <span
-                  key={index}
-                  className="text-xs font-semibold text-gray-600 bg-gray-100 px-2 py-1 rounded-xl"
-                >
-                  {tag}
-                </span>
-              ))}
-
+        <div className="pt-10 flex flex-wrap items-center gap-12 justify-around">
+          {tags &&
+            tags.map((tag, index) => (
+              <span
+                key={index}
+                className="text-xs font-semibold text-gray-600 bg-gray-100 px-2 py-1 rounded-xl"
+              >
+                {tag}
+              </span>
+            ))}
+          <Link href={`/blog/${slug}`}>
             <p className="text-xs font-semibold text-gray-600">READ ME</p>
-          </div>
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
